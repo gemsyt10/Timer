6828e3ae18949ef90bb2cc2c5ccd2a7c4b3c967f
Gemsyt10 <gemsyt10@gmail.com>
2025-12-30 13:24:14 +0200

    Update script.js
    створення кнопок збереження та завантаження

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-// Створення констант та пошук елементів  
+   
+   // Створення констант та пошук елементів 
 let isRuning = false;
 let clearintervalId;
 let timeSeconds = Number(localStorage.getItem('timeSeconds')) || 0;  
 let timeMinutes = Number(localStorage.getItem('timeMinutes')) || 0;
+let saveTimerTimeFromLoad = () => {
+    localStorage.setItem("progressS", timeSeconds);
+    localStorage.setItem("progressM", timeMinutes);
+}
 const texth1 = document.querySelector('.timer');  
 const cont = document.querySelector('.btns');
+const loadBtnCont = document.querySelector('.load-reset-btns');
         
 // Медіа файли
 const offSound = new Audio('off-btn.mp3');
@@ -71,13 +77,30 @@ function pauseTimer() {
 let btnres = '<button id="butn-reset">Reset</button>';
 let btnon = '<button id="btn-include">Start</button>';
 let btnpause = '<button id="btn-pause">Pause</button>';
+let btnSave = `<button id="save-btn">Save</button>`;
+let btnLoad = `<button id="load-btn">Load</button>`;
 
 // добавлення кнопок на сторінку
 cont.insertAdjacentHTML('beforeend', btnon);
 cont.insertAdjacentHTML('beforeend', btnres);
 cont.insertAdjacentHTML('beforeend', btnpause);
+loadBtnCont.insertAdjacentHTML('beforeend', btnSave);
+loadBtnCont.insertAdjacentHTML('beforeend',btnLoad);
         
 // додаємо функції за прослушкою события
 document.querySelector('#butn-reset').addEventListener('click', resetTimer);
 document.querySelector('#btn-include').addEventListener('click', startTimer);
 document.querySelector('#btn-pause').addEventListener('click', pauseTimer)
+document.querySelector('#save-btn').addEventListener('click', ()=> {
+  saveTimerTimeFromLoad()
+});
+document.querySelector('#load-btn').addEventListener('click', ()=>{
+   if (isRuning) {
+        clearInterval(clearintervalId)
+        isRuning = false
+    }
+   timeSeconds = Number(localStorage.getItem("progressS")) || 0;
+   timeMinutes = Number(localStorage.getItem("progressM")) || 0;
+   updateTimermessage()
+   texth1.style.animationIterationCount = 0
+})
